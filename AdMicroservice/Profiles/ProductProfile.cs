d46a5e84dfc47128d47dbd2d6fed6f074f021c2f
bojananeskovic <bojananeskovic007@gmail.com>
2021-08-08 11:34:10 +0200

    AdMicroservice - Added profiles

--- AdMicroservice/Profiles/ProductProfile.cs
+++ AdMicroservice/Profiles/ProductProfile.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using AdMicroservice.Entities;
+using AdMicroservice.Models.DTO;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AdMicroservice.Profiles
 {
-    public class ProductProfile
+    public class ProductProfile : Profile
     {
+        public ProductProfile()
+        {
+            CreateMap<Product, ItemForSaleDto>();
+
+            CreateMap<Product, ItemForSale>();
+            CreateMap<ProductCreationDto, Product>();
+            CreateMap<Product, ProductConfirmationDto>();
+
+            CreateMap<ProductUpdateDto, Product>();
+        }
     }
 }
--- AdMicroservice/Profiles/ServiceProfile.cs
+++ AdMicroservice/Profiles/ServiceProfile.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using AdMicroservice.Entities;
+using AdMicroservice.Models.DTO;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AdMicroservice.Profiles
 {
-    public class ServiceProfile
+    public class ServiceProfile : Profile
     {
+        public ServiceProfile()
+        {
+            CreateMap<Service, ItemForSaleDto>();
+
+            CreateMap<Service, ItemForSale>();
+            CreateMap<ServiceCreationDto, Service>();
+            CreateMap<Service, ServiceConfirmationDto>();
+
+            CreateMap<ServiceUpdateDto, Service>();
+        }
     }
 }
