5b5c3920cc256f916a3070f3b0fd5d6dd3fee586
Aleksandra Knezevic <aleksandra.knezevic@vega.local>
2021-09-06 23:41:16 +0200

    created db

---
+++ PurchaseMicroservice/PurchaseMicroservice/Helpers/IAuth.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PurchaseMicroservice.Helpers
+{
+    public interface IAuth
+    {
+        public bool AuthorizeUser(string key);
+    }
+}
