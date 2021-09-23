c4875b81edf29f2bdec4dd5aeae4654e7e9eff84
Stefan Ostojic <stefanostojic98@gmail.com>
2021-09-23 13:49:13 +0200

    Added Account, AccRole and City entities.

---
+++ AccountService/Entities/Account.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace AccountService.Entities
+{
+    public class Account : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public Guid RoleId { get; set; }
+        public AccRole Role { get; set; }
+        public Guid? CityId { get; set; }
+        public virtual City City { get; set; }
+        
+        public Account() : base()
+        {
+            
+        }
+    }
+}
---
+++ AccountService/Entities/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountService.Entities
+{
+    public class City
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Account> Accounts { get; set; }
+    }
+}
