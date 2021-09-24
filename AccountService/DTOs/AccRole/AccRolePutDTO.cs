5ca198b830f37e0700bb22265046857353cb93e3
Stefan Ostojic <stefanostojic98@gmail.com>
2021-09-24 03:08:32 +0200

    Added AutoMapper mapping for Roles and updated DTOs.

---
+++ AccountService/DTOs/AccRole/AccRolePutDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AccountService.DTOs.AccRole
+{
+    public class AccRolePutDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ AccountService/Profiles/AccRoleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AccountService.DTOs.AccRole;
+using AccountService.Entities;
+using AutoMapper;
+
+namespace AccountService.Profiles
+{
+    public class AccRoleProfile : Profile
+    {
+        public AccRoleProfile()
+        {
+            CreateMap<AccRolePostDTO, AccRole>();
+            CreateMap<AccRolePutDTO, AccRole>();
+            CreateMap<AccRole, AccRoleResponseDTO>();
+        }
+    }
+}
