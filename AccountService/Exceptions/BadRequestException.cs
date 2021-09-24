62b7134c04a8cac01581e061f2fda622a4025e49
Stefan Ostojic <stefanostojic98@gmail.com>
2021-09-24 02:20:38 +0200

    Added custom exceptions.

---
+++ AccountService/Exceptions/BadRequestException.cs
@@ -0,0 +1,18 @@
+﻿namespace AccountService.Exceptions
+{
+    public class BadRequestException : CustomException
+    {
+        public BadRequestException()
+        : base()
+        {
+            StatusCode = 400;
+            ErrorMessage = "Bad request.";
+        }
+
+        public BadRequestException(string message)
+            : this()
+        {
+            ErrorMessage = message;
+        }
+    }
+}
---
+++ AccountService/Exceptions/CustomException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccountService.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public CustomException()
+        {
+            StatusCode = 500;
+            ErrorMessage = "Internal server error.";
+        }
+    }
+}
